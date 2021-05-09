aff1d9bd0ef724b5c25a2dccc617f3ee07a7cc22
roamschemer <roamschemer@gmail.com>
2021-05-10 02:20:25 +0900

    Program.cs を修正してグラフの表示に成功した

--- Program.cs
+++ Program.cs
@@ -16,7 +16,9 @@ namespace BlazoriseChartsTry {
             builder.RootComponents.Add<App>("#app");
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-            builder.Services.AddEmptyProviders();
+            builder.Services.AddBlazorise(options => {
+                options.ChangeTextOnKeyPress = true;
+            }).AddEmptyProviders();
 
             await builder.Build().RunAsync();
         }
