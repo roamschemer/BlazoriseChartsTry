7963c605f25262f2854cce769f9dd1795cf3c99e
roamschemer <roamschemer@gmail.com>
2021-05-11 23:11:56 +0900

    ScatterChart追加

--- Program.cs
+++ Program.cs
@@ -8,6 +8,7 @@ using System.Collections.Generic;
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace BlazoriseChartsTry {
     public class Program {
